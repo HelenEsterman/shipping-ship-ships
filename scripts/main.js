23986b3c6ddcf5bfb9ca8a61fefff68ae37b0ce4
Helen Esterman <helenesterman99@gmail.com>
2023-08-01 16:15:24 -0400

    added a tiny bit of css styling

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,16 @@ let htmlContainer = document.querySelector(".container")
 
 const htmlContent = 
     `<h1>Shipping Ship Ships</h1>
-    <article>
-        <section> 
+    <article class="details">
+        <section class="detail--column"> 
             <h2>List of Cargo Ships</h2>
             ${CargoShipList()}
         </section>
-        <section>
+        <section class="detail--column">
             <h2>Haulers</h2>
             ${HaulersList()}
         </section>
-        <section>
+        <section class="detail--column">
             <h2>Docks</h2>
             ${DockList()}
         </section>
