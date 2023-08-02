a9191e4a8accbbfdd4f01cef78f137fdb963def9
Helen Esterman <helenesterman99@gmail.com>
2023-08-02 11:08:32 -0400

    finally got rid of comma at the end of my docks list window alert

--- scripts/docks.js
+++ scripts/docks.js
@@ -33,6 +33,7 @@ document.addEventListener(
         let dockedHauler = ""
 
         //iterate the array of hauler objects
+        
         for (const hauler of allHaulers) {
         
             //does the dock id foreign key match the id of the current dock
@@ -41,6 +42,8 @@ document.addEventListener(
             //reassign default variable of dockedHauler
             //if the id of a dock only matches one hauler then i want to add that hauler to the dockedHauler variable
                 dockedHauler += `${hauler.name}, `
+
+                // concatenatedString.substring(0, concatenatedString.length - 1)
             }
             //if the id of a dock matches the dock id of more than one hauler then i want to add BOTH to dockedHauler variable
             //if the id of a dock never matches any dock id of a hauler then i want to add string "nothing" to dockedHauler variable
@@ -49,7 +52,8 @@ document.addEventListener(
                 window.alert(`The ${dockLocation} dock is currently unloading nothing`)
             }else{
         //show alert with specific format
-        window.alert(`The ${dockLocation} dock is currently unloading ${dockedHauler}`)
+        const dockedHauler2 = dockedHauler.slice(0, dockedHauler.length - 2)
+        window.alert(`The ${dockLocation} dock is currently unloading ${dockedHauler2}`)
             }
         } 
     }
